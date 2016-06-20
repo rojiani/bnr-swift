d61e5e35a3b8385e6567c6a72a09c45d76a3971e
Navid Rojiani <nrojiani@gmail.com>
2016-06-19 23:17:40 -0700

    Ch. 15 - Structs & Classes

--- 15-StructsAndClasses/MonsterTown/MonsterTown/Vampire.swift
+++ 15-StructsAndClasses/MonsterTown/MonsterTown/Vampire.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class Vampire : Monster {
+    
+    static var vampireThralls = [Vampire]()
+    
+    override func terrorizeTown() {
+        if (town?.population > 1) {
+            town?.changePopulation(-1)
+        }
+        Vampire.vampireThralls.append(self)
+        super.terrorizeTown()
+    }
+    
+    func changeName(name: String) {
+        self.name = name
+    }
+    
+    func printVampireThralls() -> () {
+        print("Vampire Thralls:")
+        for vampire in Vampire.vampireThralls {
+            print(vampire.name)
+        }
+    }
+}
--- 15-StructsAndClasses/MonsterTown/MonsterTown/main.swift
+++ 15-StructsAndClasses/MonsterTown/MonsterTown/main.swift
@@ -19,3 +19,17 @@ fredTheZombie.town = myTown
 fredTheZombie.terrorizeTown()
 fredTheZombie.town?.printTownDescription()
 fredTheZombie.changeName("Fred the Zombie", walksWithLimp: false)
+
+/* Silver Challenge */
+let dracula = Vampire()
+let nosferatu = Vampire()
+dracula.town = myTown
+nosferatu.town = myTown
+dracula.changeName("Dracula")
+nosferatu.changeName("Nosferatu")
+dracula.terrorizeTown()
+dracula.town?.printTownDescription()
+dracula.printVampireThralls()
+nosferatu.terrorizeTown()
+nosferatu.town?.printTownDescription()
+nosferatu.printVampireThralls()
