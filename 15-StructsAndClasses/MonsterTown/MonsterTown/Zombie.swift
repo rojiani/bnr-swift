65d120b25f8323b7c17fcd3ae2ca5db0bb6a04cb
Navid Rojiani <nrojiani@gmail.com>
2016-06-19 19:42:12 -0700

    Ch. 15: Structs section

---
+++ 15-StructsAndClasses/MonsterTown/MonsterTown/Zombie.swift
@@ -0,0 +1,9 @@
+//
+//  Zombie.swift
+//  MonsterTown
+//
+//  Created by Navid Rojiani on 6/19/16.
+//  Copyright © 2016 nkroji. All rights reserved.
+//
+
+import Foundation
---
+++ 15-StructsAndClasses/MonsterTown/MonsterTown/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Navid Rojiani on 6/13/16.
+//  Copyright © 2016 nkroji. All rights reserved.
+//
+
+import Foundation
+
+/* Structs */
+var myTown = Town()
+myTown.changePopulation(500)
+myTown.printTownDescription()
+
