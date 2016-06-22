4a7b8949e047341460a1b9734c4db8359e794f18
Navid Rojiani <nrojiani@gmail.com>
2016-06-21 20:58:23 -0700

    lazy properties with IIFE closure

--- 16-Properties/MonsterTown/MonsterTown/Town.swift
+++ 16-Properties/MonsterTown/MonsterTown/Town.swift
@@ -14,6 +14,28 @@ struct Town {
     var population = 5422
     var numberOfStoplights = 4
     
+    enum Size {
+        case Small
+        case Medium
+        case Large
+    }
+    
+    // IIFE - called when townSize is referenced
+    // the () along with 'lazy' marking ensure that Swift will call the
+    // closure & assign the result it returns to townSize when the property is acessed for 
+    // the first time
+    lazy var townSize: Size = {
+        switch self.population {
+        case 0...10000:
+            return Size.Small
+        
+        case 10001...100000:
+            return Size.Medium
+        
+        default:
+            return Size.Large
+        }
+    }()
     
     // Methods
     func printTownDescription() {
@@ -25,6 +47,7 @@ struct Town {
     mutating func changePopulation(amount: Int) {
         population += amount
     }
+    
 }
 
 
