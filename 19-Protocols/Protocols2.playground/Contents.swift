0863c67f0f074a26a65a03049edd18605a158d76
Navid Rojiani <nrojiani@gmail.com>
2016-07-10 18:25:52 -0700

    Ch. 19, pre-Challenges

---
+++ 19-Protocols/Protocols2.playground/Contents.swift
@@ -0,0 +1,129 @@
+
+import UIKit
+
+
+protocol TabularDataSource {
+    /* Computed Properties */
+    var numberOfRows: Int { get }
+    var numberOfColumns: Int { get }
+    
+    func labelForRow(row: Int) -> String
+    func labelForColumn(column: Int) -> String
+    
+    func itemForRow(row: Int, column: Int) -> Int
+}
+
+
+struct Person {
+    let name: String
+    let age: Int
+    let yearsOfExperience: Int
+}
+
+struct Department: TabularDataSource, CustomStringConvertible {
+    /* Stored Properties */
+    let name: String
+    var people = [Person]()
+    
+    /* Computed Properties */
+    var description: String {
+        return "Department (\(name))"
+    }
+    
+    var numberOfRows: Int {
+        return people.count
+    }
+    
+    var numberOfColumns: Int {
+        return 2
+    }
+    
+    /* Initializers */
+    init(name: String) {
+        self.name = name
+    }
+    
+    /* Methods */
+    mutating func addPerson(person: Person) {
+        people.append(person)
+    }
+    
+    func labelForRow(row: Int) -> String {
+        return people[row].name
+    }
+    
+    func labelForColumn(column: Int) -> String {
+        switch column {
+        case 0: return "Age"
+        case 1: return "Years of Experience"
+        default: fatalError("Invalid column!")
+        }
+    }
+    
+    func itemForRow(row: Int, column: Int) -> Int {
+        let person = people[row]
+        switch column {
+        case 0: return person.age
+        case 1: return person.yearsOfExperience
+        default: fatalError("Invalid column!")
+        }
+    }
+}
+
+func printTable(dataSource: protocol<TabularDataSource, CustomStringConvertible>) -> () {
+    print("Table: \(dataSource)")
+    
+    // Create arrays of the row and column labels
+    let rowLabels = (0 ..< dataSource.numberOfRows).map { dataSource.labelForRow($0) }
+    let columnLabels = (0 ..< dataSource.numberOfColumns).map { dataSource.labelForColumn($0) }
+    
+    // Create an array of the width of each row label
+    let rowLabelWidths = rowLabels.map { $0.characters.count }
+    
+    // Determine the length of longest row label
+    guard let maxRowLabelWidth = rowLabelWidths.maxElement() else {
+        return
+    }
+    
+    // Create first row containing column headers
+    var firstRow: String = padding(maxRowLabelWidth) + " |"
+    
+    // Also keep track of the width of each column
+    var columnWidths = [Int]()
+    
+    for columnLabel in columnLabels {
+        let columnHeader = " \(columnLabel) |"
+        firstRow += columnHeader
+        columnWidths.append(columnHeader.characters.count)
+    }
+    print(firstRow)
+    
+    for i in 0 ..< dataSource.numberOfRows {
+        // Pad the row label out so they are all the same length
+        let paddingAmount = maxRowLabelWidth - rowLabelWidths[i]
+        var out = rowLabels[i] + padding(paddingAmount) + " |"
+        
+        // Append each item in this row to our string
+        for j in 0 ..< dataSource.numberOfColumns {
+            let item = dataSource.itemForRow(i, column: j)
+            let itemString = " \(item) |"
+            let paddingAmount = columnWidths[j] - itemString.characters.count
+            out += padding(paddingAmount) + itemString
+        }
+        
+        // Done - print it!
+        print(out)
+    }
+}
+
+/* return string of specified length */
+func padding(amount: Int) -> String {
+    return String(count: amount, repeatedValue: Character(" "))
+}
+
+var department = Department(name: "Engineering")
+department.addPerson(Person(name: "Joe", age: 30, yearsOfExperience: 6))
+department.addPerson(Person(name: "Karen", age: 40, yearsOfExperience: 18))
+department.addPerson(Person(name: "Fred", age: 50, yearsOfExperience: 20))
+
+printTable(department)
