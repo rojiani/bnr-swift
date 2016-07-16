e6c4f1d8231d53578716982844d745cb1d0d3ffc
Navid Rojiani <nrojiani@amazon.com>
2016-07-15 20:08:04 -0700

    Ch. 23 - Protocol Extensions

--- 23-ProtocolExtensions/ProtocolExtensions.playground/Contents.swift
+++ 23-ProtocolExtensions/ProtocolExtensions.playground/Contents.swift
@@ -5,17 +5,26 @@ protocol ExerciseType: CustomStringConvertible {
     var name: String { get }
     var caloriesBurned: Double { get }
     var minutes: Double { get }
+    var title: String { get }
 }
 
+// Protocol extension
 extension ExerciseType {
-    // Default implementation for ExerciseType
+    // Default implementation - must be in a protocol *extension*
     var description: String {
         return "Exercise(\(name), burned \(caloriesBurned) calories in \(minutes) minutes)"
     }
 }
 
+extension ExerciseType {
+    var title: String {
+        return "\(name) - \(minutes) minutes"
+    }
+}
+
 struct EllipticalTrainer: ExerciseType {
     let name = "Elliptical Machine"
+    let title = "Go Fast Elliptical Machine 3000"
     let caloriesBurned: Double
     let minutes: Double
 }
@@ -29,6 +38,11 @@ struct Treadmill: ExerciseType {
     let minutes: Double
     
     let distanceInMiles: Double
+
+    // Default implementation for 'description' can be overridden
+    var description: String {
+        return "Treadmill (\(caloriesBurned) calories and \(distanceInMiles) miles in \(minutes) minutes)"
+    }
 }
 
 let runningWorkout = Treadmill(caloriesBurned: 350,
@@ -69,3 +83,8 @@ extension SequenceType where Generator.Element == ExerciseType {
 let mondayWorkout: [ExerciseType] = [ellipticalWorkout, runningWorkout]
 print(mondayWorkout.totalCaloriesBurned())
 
+for exercise in mondayWorkout {
+    print(exercise.title)
+}
+
+print(ellipticalWorkout.title)
