89322c29b73b9fe97a5f2ed6a83180c71c68b4d6
Navid Rojiani <nrojiani@gmail.com>
2016-07-27 23:20:23 -0700

    UITableViewDataSource protocol

--- 27-iOS/iTahDoodle/iTahDoodle/TodoList.swift
+++ 27-iOS/iTahDoodle/iTahDoodle/TodoList.swift
@@ -17,3 +17,27 @@ class TodoList: NSObject {
     }
 
 }
+
+extension TodoList: UITableViewDataSource {
+
+    func tableView(tableView: UITableView,
+                   cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        // Dequeue a reusable cell with the id "Cell" & the given IndexPath
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let item = items[indexPath.row]
+
+        // force unwrap textLabel: not all UITableViewCells are guaranteed to have a textLabel, but
+        // the one you are using does
+        cell.textLabel!.text = item
+
+        return cell
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // one row for each to-do item
+        return items.count
+    }
+
+}
